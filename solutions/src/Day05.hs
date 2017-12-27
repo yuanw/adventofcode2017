5099c9afe380acf38b2a139979bacac0c872cd03
Yuan Wang <yuan.w0811@gmail.com>
2017-12-26 22:03:50 -0600

    remove dupcated code

--- solutions/src/Day05.hs
+++ solutions/src/Day05.hs
@@ -19,10 +19,10 @@ data Maze = Maze { step :: Int
 day05 :: IO ()
 day05 = do
     content <- readFile "input5.txt"
-    -- putStrLn "Part I"
-    -- (print . step) $ until hasExit jump (readInput content)
+    putStrLn "Part I"
+    (print . step) $ until hasExit (jump  (\x -> x +1)) (readInput content)
     putStrLn "Part II"
-    (print . step) $ until hasExit jumpII (readInput content)
+    (print . step) $ until hasExit (jump  (\x -> if x >= 3 then x - 1 else x + 1) )  (readInput content)
 
 
 readInput :: String -> Maze
@@ -32,19 +32,13 @@ readInput content = Maze {step=0, current=0, jumps= (Seq.fromList . map readF . 
 hasExit :: Maze -> Bool
 hasExit maze = current maze >= Seq.length (jumps maze)
 
-jump :: Maze -> Maze
-jump maze = if hasExit maze then maze else Maze {step = step maze + 1, current=newCurrent, jumps=newJumps}
+jump ::  (Int -> Int) -> Maze -> Maze
+jump modifier maze = if hasExit maze then maze else Maze {step = step maze + 1, current=newCurrent, jumps=newJumps}
     where index = current maze
           move = Seq.index (jumps maze) index
           newCurrent = current maze + move
-          newJumps = Seq.update index (move + 1) (jumps maze)
+          newJumps = Seq.update index (modifier move) (jumps maze)
 
-jumpII :: Maze -> Maze
-jumpII maze = if hasExit maze then maze else Maze {step = step maze + 1, current=newCurrent, jumps=newJumps}
-    where index = current maze
-          move = Seq.index (jumps maze) index
-          newCurrent = current maze + move
-          newJumps = Seq.update index (f move) (jumps maze)
-          f x = if (x >= 3) then x - 1 else x + 1
+
 
 
